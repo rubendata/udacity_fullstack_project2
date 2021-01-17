c0fcabc1fe3a01e03410e36bf620868d847d651c
rubendata <info@rubensimon.de>
2021-01-17 13:25:25 +0100

    installed cors and added to app

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -16,7 +16,14 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
